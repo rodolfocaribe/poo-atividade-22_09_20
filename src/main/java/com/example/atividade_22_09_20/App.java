1f30448ba38e31b6bed51cf1fe63204be90ab449
RODOLFO <rodolfo.ribeiro@estudante.iftm.edu.br>
2022-09-20 20:37:02 -0300

    Criando a classe Elevador com atributos, getters, setters e construtor.

---
+++ src/main/java/com/example/atividade_22_09_20/App.java
@@ -0,0 +1,10 @@
+package com.example.atividade_22_09_20;
+
+public class App {
+    public static void simulador () {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/example/atividade_22_09_20/Elevador.java
+++ src/main/java/com/example/atividade_22_09_20/Elevador.java
@@ -1,4 +1,46 @@
 package com.example.atividade_22_09_20;
 
 public class Elevador {
+    public int getAndarAtual() {
+        return andarAtual;
+    }
+
+    public void setAndarAtual(int andarAtual) {
+        this.andarAtual = andarAtual;
+    }
+
+    public int getTotalAndares() {
+        return totalAndares;
+    }
+
+    public void setTotalAndares(int totalAndares) {
+        this.totalAndares = totalAndares;
+    }
+
+    public int getCapacidadePessoas() {
+        return capacidadePessoas;
+    }
+
+    public void setCapacidadePessoas(int capacidadePessoas) {
+        this.capacidadePessoas = capacidadePessoas;
+    }
+
+    public int getPessoasDentro() {
+        return pessoasDentro;
+    }
+
+    public void setPessoasDentro(int pessoasDentro) {
+        this.pessoasDentro = pessoasDentro;
+    }
+
+    private int andarAtual = 0;
+    private int totalAndares;
+    private int capacidadePessoas;
+    private int pessoasDentro = 0;
+
+    public Elevador(int capacidadePessoas, int andarAtual) {
+        this.capacidadePessoas = capacidadePessoas;
+        this.andarAtual = andarAtual;
+    }
+
 }
