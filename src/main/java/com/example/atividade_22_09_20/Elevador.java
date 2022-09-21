72b6f1ceec42b22680462ad8e02726077911a145
RODOLFO <rodolfo.ribeiro@estudante.iftm.edu.br>
2022-09-20 21:14:25 -0300

    Cria todos os demais metodos da classe Elevador

--- src/main/java/com/example/atividade_22_09_20/Elevador.java
+++ src/main/java/com/example/atividade_22_09_20/Elevador.java
@@ -1,5 +1,7 @@
 package com.example.atividade_22_09_20;
 
+import javax.swing.JOptionPane;
+
 public class Elevador {
     public int getAndarAtual() {
         return andarAtual;
@@ -34,13 +36,49 @@ public class Elevador {
     }
 
     private int andarAtual = 0;
-    private int totalAndares;
-    private int capacidadePessoas;
+    private int totalAndares = 12;
+    private int capacidadePessoas = 4;
     private int pessoasDentro = 0;
 
     public Elevador(int capacidadePessoas, int andarAtual) {
         this.capacidadePessoas = capacidadePessoas;
         this.andarAtual = andarAtual;
+    }
+
+    public boolean entrar() {
+        if (capacidadePessoas < 4) {
+            capacidadePessoas++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean sair() {
+        if (capacidadePessoas > 0) {
+            capacidadePessoas--;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean subir() {
+        if (andarAtual < 12) {
+            andarAtual++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean descer() {
+        if (andarAtual > 1) {
+            andarAtual--;
+            return true;
+        }
+        return false;
+    }
+
+    public void exibir() {
+        JOptionPane.showMessageDialog(null, "Bem vindo ao Elevator Controller!\nEste prédio possui " + totalAndares + " andares.\nA capacidade deste elevador é de " + capacidadePessoas + " pessoas.\nNeste momento o elevador está com " + pessoasDentro + " pessoas.\nO elevador está no " + andarAtual + " andar.");
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.atividade_22_09_20 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.atividade_22_09_20 to javafx.fxml;
